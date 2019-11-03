baad81f9774828f93620e794d8aae9c7ea30d5f6
syedfaridul <Faridul123>
2019-11-03 19:28:57 +0600

    Some Update 03112019

---
+++ app/src/main/java/blog/video/biswas/londonpundrosurvey/DateTimeHandler.java
@@ -0,0 +1,4 @@
+package blog.video.biswas.londonpundrosurvey;
+
+public class DateTimeHandler {
+}
--- app/src/main/java/blog/video/biswas/londonpundrosurvey/Login.java
+++ app/src/main/java/blog/video/biswas/londonpundrosurvey/Login.java
@@ -1,14 +1,137 @@
 package blog.video.biswas.londonpundrosurvey;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import blog.video.biswas.londonpundrosurvey.databinding.ActivityLoginBinding;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Login extends AppCompatActivity {
+
+    DateTimeHandler TimeHandler;
+    DBHelper DatabaseHelper;
+
+    Globals Global;
+    Cursor cursor;
+
+
+    private ActivityLoginBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        DatabaseHelper = new DBHelper();
+        TimeHandler = new DateTimeHandler();
+
+
+        binding.tvregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //login(binding.edtemail.getText().toString(), binding.edtpassword.getText().toString());
+
+
+                //************************************************
+                try {
+                    if (binding.edtpassword.getText().toString().trim().length() == 0) {
+                        AlertDialog DiaOK = DialogOK();
+                        DiaOK.setMessage("Please enter your password");
+                        DiaOK.show();
+                        binding.edtpassword.requestFocus();
+                        return;
+                    } else {
+                        if (binding.edtemail.getText().toString().trim().length() == 0) {
+                            AlertDialog DiaOK = DialogOK();
+                            DiaOK.setMessage("Please select the TAB ID");
+                            DiaOK.show();
+                            binding.edtemail.requestFocus();
+                            return;
+                        } else {
+                            cursor = login(binding.edtemail.getText().toString(), binding.edtpassword.getText().toString();
+                            if (cursor.moveToFirst()) {
+                                do {
+                                    // Password validation
+                                    String PasswordDB = cursor.getString(cursor.getColumnIndex("Password"));
+
+                                }
+                                while (cursor.moveToNext());
+                            }
+                            cursor.close();
+                        }
+                    }
+
+                } catch (
+                        Exception e) {
+                    AlertDialog DiaOK = DialogOK();
+                    DiaOK.setMessage(e.toString() + " SignIn button click");
+                    DiaOK.show();
+                }
+
+            }
+        });
+
+
+    }
+
+    //        });
+//                }
+    private void login(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Log.i("registrationLog", "Fail to login");
+                }
+
+            }
+        });
+    }
+    private AlertDialog DialogOK()
+    {
+        AlertDialog DialogBox = new AlertDialog.Builder(this)
+                .setTitle("Login")
+                .setMessage("")
+                .setIcon(R.drawable.name)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton)
+                    {
+
+                    }
+                })
+                .create();
+        return DialogBox;
     }
 }
